fda83217ebfde09000d4d30542af43987014ac14
maksim.vesnin <gerasimov321@gmail.com>
2023-07-09 15:25:18 +0300

    Has troubles with multiple Ninject modules loading

--- Monolythium.Core/OrderProcessing/OrderProcessingModule.cs
+++ Monolythium.Core/OrderProcessing/OrderProcessingModule.cs
@@ -11,6 +11,7 @@ namespace Monolythium.Core.OrderProcessing
 
             Bind<IOrderMaker>().To<OrderMaker>();
             Bind<IOrderAnnihilator>().To<OrderAnnihilator>();
+
         }
     }
 }
